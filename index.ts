0a3288619ca222f3232d491a83ec8052d7ab76dc
whitebear <woojs0505@naver.com>
2021-10-10 17:38:33 +0900

    Studied ?

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("hello");
+const name = "Nicolas",
+  age = 24,
+  gender = "male";
+
+
+// gender? 와 같이 ?를 붙이면 선택적인 파라미터.
+const sayHi = (name, age, gender?) => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender} :)`);
+}
+
+sayHi(name, age);
+
+export {};
