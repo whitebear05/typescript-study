e9dc827312bf4db0793d34ee0b5e0691c15f99f4
whitebear <woojs0505@naver.com>
2021-10-10 19:22:25 +0900

    Update: isBlockValid and addBlock

--- dist/index.js
+++ dist/index.js
@@ -31,6 +31,7 @@ const createNewBlock = (data) => {
     const newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
     return newBlock;
 };
+const getHashforBlock = (aBlock) => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
 const isBlockValid = (candidateBlock, previousBlock) => {
     if (Block.validateStructure(candidateBlock)) {
         return false;
@@ -40,6 +41,17 @@ const isBlockValid = (candidateBlock, previousBlock) => {
     }
     else if (previousBlock.hash !== candidateBlock.previousHash) {
         return false;
+    }
+    else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+        return false;
+    }
+    else {
+        return true;
+    }
+};
+const addBlock = (candidateBlock) => {
+    if (isBlockValid(candidateBlock, getLatestBlock())) {
+        blockchain.push(candidateBlock);
     }
 };
 //# sourceMappingURL=index.js.map
