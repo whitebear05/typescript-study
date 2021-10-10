6ba1d98f38cfb8214ed57417b52d8a25a442ddd8
whitebear <woojs0505@naver.com>
2021-10-10 18:19:44 +0900

    Update: block and blockchain

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,15 @@
 "use strict";
-// 인터페이스(interface)를 사용하면 ts 측면에서 좀 더 안전하지만,
-// react, node, express를 사용한다면 코드에서 클래스를 사용해야 한다.
 Object.defineProperty(exports, "__esModule", { value: true });
-class Human {
-    constructor(name, age, gender) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    constructor(index, hash, previousHash, data, timestamp) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
-const lynn = new Human("Lynn", 18, "female");
-// gender? 와 같이 ?를 붙이면 선택적인 파라미터.
-const sayHi = (person) => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender} !!`;
-};
-console.log(sayHi(lynn));
+const genesisBlock = new Block(0, "2020202003423", "", "Hello", 123456);
+let blockchain = [genesisBlock];
+console.log(blockchain);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,22 @@
-// 인터페이스(interface)를 사용하면 ts 측면에서 좀 더 안전하지만,
-// react, node, express를 사용한다면 코드에서 클래스를 사용해야 한다.
-
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(index: number, hash: string, previousHash: string, data: string, timestamp: number) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const lynn = new Human("Lynn", 18, "female")
+const genesisBlock: Block = new Block(0, "2020202003423", "", "Hello", 123456);
 
-// gender? 와 같이 ?를 붙이면 선택적인 파라미터.
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender} !!`;
-}
+let blockchain: [Block] = [genesisBlock];
 
-console.log(sayHi(lynn));
+console.log(blockchain);
 
 export {};
